8d6a096aca5b5aec9a12609a2a697daa06058340
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-15 23:52:57 +0530

    dataTypes and ecma standards

---
+++ 03_ecma.js
@@ -0,0 +1,24 @@
+"use strict";   //treat document as newer version
+
+// alert(2+2)  //this is a node js not browser
+
+/*
+types of data
+int  1 2 3 34 
+big int large no.
+boolean true/false
+string ""
+symbol unique
+null standalone 
+undefined
+*/
+let a=12321
+a=124322344343432342343424234343423443344n
+a=true
+a="shubham"
+// a=null
+// a;
+
+// console.log(typeof a);
+// console.log(typeof null);
+console.log(typeof undefined);
