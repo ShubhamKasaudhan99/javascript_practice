04f8bd3dadc7ed1eac53dbfe7ee3a766f7d208ca
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-16 00:11:17 +0530

    data type conversions and confusions.

---
+++ 04_dataTypeConversion1.js
@@ -0,0 +1,35 @@
+// let score=33
+
+// let score="33"
+// let score="33.222"
+// let score="33a"
+// let score="a"
+// let score="a2"
+// let score=null
+// let score=undefined
+// let score=true
+// let score=false
+
+
+// console.log(typeof score);
+
+// let isNum=Number(score)
+// console.log(isNum);
+
+
+// let a=1
+// let a=""
+// let a="shubham"
+// let a="123"
+// let a=0
+// let a="0"
+
+// let isBool=Boolean(a)
+// console.log(isBool);
+
+
+// let st=123
+// let st=0
+// let st=ab
+let n=String(st)
+console.log(n);
