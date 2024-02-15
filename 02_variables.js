1bb468d1804d00913e8647b3b59df72676df8b2b
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-15 23:37:00 +0530

    variables and declaration key words in javascript.

---
+++ 02_variables.js
@@ -0,0 +1,23 @@
+const accountId=12345
+let accountName="shubham"
+var accountEmail="shubham@gmail.com"
+accountCity="kolkata"
+let accountState;
+
+// console.log(accountEmail);
+// console.log(accountEmail,accountId,accountName,accountCity);
+
+// console.log([accountEmail,accountId,accountName,accountCity]);
+
+//tabular form of the data
+console.table([accountEmail,accountId,accountName,accountCity,accountState]);
+
+/*
+var is not used beacuse of it is not understand of scope and functional scoping.
+let is new version of declaration of variable.
+const is constant key word
+
+{
+    is called a scope
+}
+*/
