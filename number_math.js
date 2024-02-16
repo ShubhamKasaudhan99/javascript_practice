3f2a0d315e1c0ff53f53376eebc88db668665aab
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-16 18:59:12 +0530

    numbers and maths methods.

---
+++ number_math.js
@@ -0,0 +1,34 @@
+const score=6743
+console.log(score);
+const balance=new Number(873)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+// balance=43.43
+// console.log(balance.toString().length);
+
+const s=423.23
+// console.log(s.toFixed(1));
+
+const otherNumber=32322.34243
+// console.log(otherNumber.toPrecision(4));
+
+let hundreds=1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-32));
+// console.log(Math.round(3.6));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.4));
+
+// console.log(Math.max(2,2,3,24,65));
+// console.log(Math.min(2,2,3,24,65));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10+1);
+// console.log(Math.floor(Math.random()*10+1));
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min));
