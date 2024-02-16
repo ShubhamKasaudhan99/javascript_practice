c480eced650c3ec3479cf9655e940cb2da58369a
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-16 17:11:55 +0530

    Add files via upload

---
+++ 05_dataTypeConversion.js
@@ -0,0 +1,18 @@
+// let value=3
+// let neg=-value
+// console.log(neg);
+
+// let str1="shubham"
+// let str2=" kasaudhan"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log(1+"2");
+console.log("1"+2+3);
+console.log(1+2+3);
+
+let game=100
+game++
+console.log(game);
