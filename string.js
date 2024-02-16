48f637d72cb0bfc82e936799308e4d9363063270
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-16 18:30:40 +0530

    strings and methods.

---
+++ string.js
@@ -0,0 +1,29 @@
+const name="shubham"
+const repo=50
+// console.log(name+repo+"value");
+
+// console.log(`hello my name is ${name} and my repo is ${repo}`);
+
+const str=new String("gamefdsgfddfgvdfcv")
+// console.log(str);
+// console.log(str[0]);
+// console.log(str.__proto__);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.charAt(6));
+
+
+// console.log(str.substring(0,5));
+// console.log(str.substring(5,-10));
+
+// console.log(str.slice(0,5));
+// console.log(str.slice(0,-5));
+
+const s="     djksvdjskfvjksd     "
+console.log(s.trim());
+const st="https://shubhamkasaudhan%20dsds.com"
+console.log(st.replace('%20','-'));
+console.log(st.includes('%20'));
+
+console.log(st.split('s'));
