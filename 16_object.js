6873185811ed65aad747ff449838b22fe70e4575
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-17 14:38:08 +0530

    object destructuring in js.

---
+++ 16_object.js
@@ -0,0 +1,28 @@
+//object destructuring
+const course={
+    name:"javascript",
+    price:"999",
+    courseinstructor:"shubham kasaudhan",
+}
+//course.courseinstructor
+
+// const {courseinstructor}=course
+// console.log(courseinstructor);
+const {courseinstructor:ins}=course
+// console.log(ins);
+
+//api
+// {
+//     "name":"shubham",
+//     "price":"free",
+//     "course":"js"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+//random me api
+//json formatter for view the api with beauty
