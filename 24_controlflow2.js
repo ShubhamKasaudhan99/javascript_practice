4e101e0ee84fb5e7d7e2dc7447a15323cb22a1e3
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-02-18 23:45:46 +0530

    control flow statement part2 in js.

---
+++ 24_controlflow2.js
@@ -0,0 +1,23 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=4
+switch(month){
+    case 1:
+    console.log("jan");
+    break;
+    case 2:
+    console.log("feb");
+    break;
+    case 3:
+    console.log("march");
+    break;
+    default:
+        console.log("enter b/w 1 to 3");
+}
