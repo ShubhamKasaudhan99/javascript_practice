0269fcec167088da3da025d9659c9154662fd8d7
shubham_kasaudhan <145333914+ShubhamKasaudhan99@users.noreply.github.com>
2024-03-01 22:11:44 +0530

    while and doWhile Loop in js.

---
+++ 27_while_dowhile.js
@@ -0,0 +1,19 @@
+index=0
+// while (index<11) {
+//     console.log(`value is ${index}`);
+//     index=index+2
+// }
+
+let arr=["shubham","sdr","bnz","vglj"]
+i=0
+// while (i<arr.length) {
+//     console.log(`value is ${arr[i]}`);
+//     i=i+1
+// }
+
+// let score=0
+let score=11
+do {
+    console.log(score);
+    score++
+} while (score<10);
